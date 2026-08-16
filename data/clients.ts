9b57d47a20434f3d42996f7f9b4e219ef73a70df
Abdelrahman Awad <abdelrahmanaawad.dev@gmail.com>
2026-08-16 17:03:48 +0300

    add new client to the list

--- data/clients.ts
+++ data/clients.ts
@@ -12,6 +12,7 @@ export const clients: Client[] = [
   { id: 'marven',  name: 'Marven Stores',  category: 'Footwear Brand',            logo: '/brands/marven.png' },
   { id: 'sparko',  name: 'Sparko',         category: 'Accessories',               logo: '/brands/sparko.png' },
   { id: 'beautiful', name: "It's Beautiful", category: 'Cosmetics & Fragrances' },
+  { id: 'firstorder', name: 'One Bond', category: 'Cosmetics',              logo: '/brands/oneBond.jpg' },
   { id: 'garden',  name: 'Garden Store',   category: 'Beauty & Personal Care' },
   { id: 'point',   name: 'Point Chemical', category: 'Automotive Spare Parts',    logo: '/brands/point.jpg' },
 ]
